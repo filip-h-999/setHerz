e5d788eb9d6f9432373856152a13d09ee1ba6fb4
filip-h-999 <filip.hrenovcik@gmail.com>
2024-03-13 20:48:02 +0100

    Refactor

--- setHz.py
+++ setHz.py
@@ -1,21 +1,23 @@
 import pyautogui
 import subprocess
 import time
+import keyboard
+import win32api
 
 
 # x, y = pyautogui.position()
 # print(f"Current Cursor Position - X: {x}, Y: {y}")
 
 # coordinatesAdvanced = X: 1165, Y: 897
-#coordinatesHz = 1504, Y: 630
-#coordinatesHz165 = X: 1504, Y: 528
-#coordinatesHz75 = X: 1470, Y: 634 / 1493, Y: 742
-#keepChanges = X: 936, Y: 613
+# coordinatesHz = 1504, Y: 630
+# coordinatesHz165 = X: 1504, Y: 528
+# coordinatesHz75 = X: 1470, Y: 634 / 1493, Y: 742
+# keepChanges = X: 936, Y: 613
 
 def setHz(HzX, HzY):
-    command = 'start ms-settings:display-advanced'
-    subprocess.run(['powershell', '-Command', command])
-    
+    command = "start ms-settings:display-advanced"
+    subprocess.run(["powershell", "-Command", command])
+
     time.sleep(1)
     pyautogui.click(1165, 897)
     time.sleep(1)
@@ -25,12 +27,24 @@ def setHz(HzX, HzY):
     time.sleep(3)
     pyautogui.click(936, 613)
 
-choice = input("Whitch Herz do you want to set? : 75Hz = 2 or 165Hz = 1?")
 
-if choice == "1":
-    setHz(1504, 528)    
-elif choice == "2":
-    setHz(1493,742)
-else:
-    print(f"Please enter a valid number (1 or 2)")
+choice = input("Whitch Herz do you want to set? : 75Hz = a or 165Hz = d?")
+
+while choice != "a" and choice != "d" and choice != "s" and choice != "q":
+        print("Please enter a valid input!")
+        choice = input("Whitch Herz do you want to set? : 75Hz = a or 165Hz = d?")
+
+if choice == "d": # 165Hz
+    setHz(1504, 528)
+    time.sleep(1)
+    keyboard.press_and_release("alt+F4")
+    exit()
+
+elif choice == "a": # 75Hz
+    setHz(1493, 742)
+    time.sleep(1)
+    keyboard.press_and_release("alt+F4")
+    exit()
+
+elif choice == "q":
     exit()
